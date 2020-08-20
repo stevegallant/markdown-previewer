fe2b2fd2b4764b5613a29f9723e1592fa5f2f6da
Steve Gallant <sgallant4@gmail.com>
2020-08-20 11:35:33 -0400

    Updated placeholder to meet all freecodecamp project reqs

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,12 +1,10 @@
 import '../styles/styles.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import marked from 'marked';
 
 // My Components
 import Editor from './modules/Editor';
 import Previewer from './modules/Previewer';
-
 
 // Main component class
 class App extends React.Component {
@@ -34,9 +32,26 @@ class App extends React.Component {
 
 const defaultEditorText = `
   # This is a header
-  The sample markdown text in this field is all passed to the Editor child component via props.
+  The sample [markdown text](https://en.wikipedia.org/wiki/Markdown) in this field is passed to the Editor child component via props. You can clear this editor and start typing to see your markdown results in the Previewer pane.
+  Technologies used to create this app include:
+  * React JavaScript with modular components
+  * CSS
+  * Webpack & NPM
+  * Plugins: marked, DOMpurify
   ## This is a subheader
-  etc
+  The Previewer component imports DOMPurify via \`import DOMPurify from 'dompurify'\` to sanitize user-entered text from the Editor. The full code block of the Previewer component is:
+  \`\`\`javascript
+  const Previewer = (props) => {
+    let cleanHTML = DOMPurify.sanitize(marked(props.text));
+    return (
+      <div id="preview" dangerouslySetInnerHTML={{__html: cleanHTML}}></div>
+    );
+  };
+  \`\`\`
+  > **This is a bolded blockquote**
+
+  Sample image:
+  ![alt text goes here](https://ritholtz.com/wp-content/uploads/2020/03/dontpanic.jpg "Don't Panic!")
 `;
 
 ReactDOM.render(<App />, document.querySelector('#app'));
--- app/assets/scripts/modules/Editor.js
+++ app/assets/scripts/modules/Editor.js
@@ -1,6 +1,5 @@
 import React from 'react';
 const Editor = (props) => {
-
   return (
     <textarea name="editor" id="editor" cols="30" rows="10" value={props.value} onChange={props.onChange}/>
   );
