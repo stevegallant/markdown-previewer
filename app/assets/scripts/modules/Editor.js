54d0733f4c7aae33ebd0b4ed6345c5a26df70a44
Steve Gallant <sgallant4@gmail.com>
2020-08-28 15:49:16 -0400

    Add padding to textarea

--- app/assets/scripts/modules/Editor.js
+++ app/assets/scripts/modules/Editor.js
@@ -1,7 +1,7 @@
 import React from 'react';
 const Editor = (props) => {
   return (
-    <textarea name="editor" id="editor" value={props.value} onChange={props.onChange}/>
+    <textarea name="editor" id="editor" value={props.value} onChange={props.onChange} className="px-2"/>
   );
 };
 
